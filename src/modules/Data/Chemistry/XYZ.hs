794082afe3031845a0fda9b659b297d0cdc5fb53
Phillip Seeber <phillip@manaan.cluster.local>
2017-07-12 14:58:17 +0200

    Transformation to and from HMatrix Matrix added Intepolation of XYZ FIles possible

--- src/modules/Data/Chemistry/XYZ.hs
+++ src/modules/Data/Chemistry/XYZ.hs
@@ -5,7 +5,9 @@ module Data.Chemistry.XYZ
 , getElements
 , getCoords
 , printXYZ
-, getCoord2Mat
+, coord2Mat
+, mat2Coord
+, interpolate
 ) where
 import System.IO
 import Data.Attoparsec.Text.Lazy
@@ -75,6 +77,8 @@ getCoord (_, x, y, z) = (x, y, z)
 getCoords :: XYZ -> [(Double,Double,Double)]
 getCoords a = map getCoord $ xyzcontent a
 
+-- print XYZ formated to a handle
+-- using mapM_ over a list enables printing a trajectory
 printXYZ :: Handle -> XYZ ->  IO ()
 printXYZ xyzhandle xyz = do
     hPrint xyzhandle $ nAtoms xyz
@@ -86,5 +90,29 @@ printXYZ xyzhandle xyz = do
         hPrintf xyzhandle "%+16.8f\n" z) (xyzcontent xyz)
 
 -- generate HMatrix Matrix of coordinates
-getCoord2Mat :: XYZ -> BLAS.Matrix Double
-getCoord2Mat a = (nAtoms a BLAS.><3) $ concat . map (\(x, y, z) -> [x, y, z]) $ getCoords a
+coord2Mat :: XYZ -> BLAS.Matrix Double
+coord2Mat a = (nAtoms a BLAS.>< 3) $ concat . map (\(x, y, z) -> [x, y, z]) $ getCoords a
+
+-- fill XYZ with coordinates from a matrix (replacing the old ones)
+mat2Coord :: XYZ -> BLAS.Matrix Double -> XYZ
+mat2Coord a mat = XYZ { nAtoms = (nAtoms a)
+                      , comment = (comment a)
+                      , xyzcontent = content
+                      }
+    where
+        content = zipWith (\e (x, y, z) -> (e, x, y, z)) (getElements a) coords
+        coords = (listList2TupleList . mat2ListList) mat
+        
+        mat2ListList :: BLAS.Matrix Double -> [[Double]]
+        mat2ListList mat_in = [BLAS.toList (mat_in BLAS.! i) | i <- [0 .. (BLAS.rows mat_in - 1)]]
+        
+        listList2TupleList :: [[Double]] -> [(Double, Double, Double)]
+        listList2TupleList listlist = map (\[x,y,z] -> (x,y,z)) listlist
+
+-- interpolate two geometries in cartesian coordinates
+interpolate :: Int -> XYZ -> XYZ -> [XYZ]
+interpolate nImages mol1 mol2 = map (mat2Coord mol1) interpol_mats
+    where
+        interpol_mats = [mol1_coordMat * ((1 BLAS.>< 1) [(1.0 - p)]) + mol2_coordMat * ((1 BLAS.>< 1) [p]) | p <- (BLAS.toList $ BLAS.linspace nImages (0, 1 :: Double))]
+        mol1_coordMat = coord2Mat mol1
+        mol2_coordMat = coord2Mat mol2
